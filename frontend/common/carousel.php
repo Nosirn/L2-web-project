8af2c447c5fa43188360985e7c6e30842af06e6e
David Allais <davidallais2001@gmail.com>
2021-11-18 00:50:11 +0100

    Ajout Carousel

---
+++ frontend/common/carousel.php
@@ -0,0 +1,92 @@
+<div class="table-container">
+    <!--Tableau (Base Carousel)-->
+    <table class="table is-hoverable">
+        <!--Ligne Tacos-->
+        <tr>
+            <!--1ère Case/Card-->
+            <td>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-48x48">
+                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-4">Tacos 1 Viandes</p>
+                                <p class="subtitle is-6">1 Viandes au choix, 2 Sauces aux choix, Frites & 2 Sauces au choix.</p>
+                            </div>
+                        </div>
+
+                        <div class="content">
+                            <br>
+                            <time >Temps de Préparation</time>
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <!--2ème Case/Card-->
+            <td>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-48x48">
+                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-4">Tacos 2 Viandes</p>
+                                <p class="subtitle is-6">2 Viandes au choix, 2 Sauces aux choix, Frites & 2 Sauces au choix.</p>
+                            </div>
+                        </div>
+
+                        <div class="content">
+                            <br>
+                            <time >Temps de Préparation</time>
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <!--3ère Case/Card-->
+            <td>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-48x48">
+                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-4">Tacos 3 Viandes</p>
+                                <p class="subtitle is-6">3 Viandes au choix, 2 Sauces aux choix, Frites & 2 Sauces au choix.</p>
+                            </div>
+                        </div>
+
+                        <div class="content">
+                            <br>
+                            <time >Temps de Préparation</time>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </table>
+</div>
