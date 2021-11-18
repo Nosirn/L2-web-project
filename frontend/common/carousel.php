c2e8f6010f1502124def3536b81a63b13a03951e
David Allais <davidallais2001@gmail.com>
2021-11-18 01:51:04 +0100

    Uptdate carousel

--- frontend/common/carousel.php
+++ frontend/common/carousel.php
@@ -1,6 +1,11 @@
 <div class="table-container">
     <!--Tableau (Base Carousel)-->
     <table class="table is-hoverable">
+
+        <tr>
+            <th>Tacos</th>
+        </tr>
+
         <!--Ligne Tacos-->
         <tr>
             <!--1ère Case/Card-->
@@ -13,24 +18,21 @@
                     </div>
                     <div class="card-content">
                         <div class="media">
-                            <div class="media-left">
-                                <figure class="image is-48x48">
-                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                                </figure>
-                            </div>
                             <div class="media-content">
                                 <p class="title is-4">Tacos 1 Viandes</p>
-                                <p class="subtitle is-6">1 Viandes au choix, 2 Sauces aux choix, Frites & 2 Sauces au choix.</p>
+                                <p class="subtitle is-6">Tacos, 1 Viandes au choix, 2 Sauces aux choix, Frites & 2 Sauces au choix.</p>
                             </div>
                         </div>
-
                         <div class="content">
+                            <br>
+                            <button class="button is-primary is-focused is-medium">Commander</button>
                             <br>
                             <time >Temps de Préparation</time>
                         </div>
                     </div>
                 </div>
             </td>
+
             <!--2ème Case/Card-->
             <td>
                 <div class="card">
@@ -41,24 +43,21 @@
                     </div>
                     <div class="card-content">
                         <div class="media">
-                            <div class="media-left">
-                                <figure class="image is-48x48">
-                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                                </figure>
-                            </div>
                             <div class="media-content">
                                 <p class="title is-4">Tacos 2 Viandes</p>
-                                <p class="subtitle is-6">2 Viandes au choix, 2 Sauces aux choix, Frites & 2 Sauces au choix.</p>
+                                <p class="subtitle is-6">Tacos, 2 Viandes au choix, 2 Sauces aux choix, Frites & 2 Sauces au choix.</p>
                             </div>
                         </div>
-
                         <div class="content">
+                            <br>
+                            <button class="button is-primary is-focused is-medium">Commander</button>
                             <br>
                             <time >Temps de Préparation</time>
                         </div>
                     </div>
                 </div>
             </td>
+
             <!--3ère Case/Card-->
             <td>
                 <div class="card">
@@ -69,18 +68,178 @@
                     </div>
                     <div class="card-content">
                         <div class="media">
-                            <div class="media-left">
-                                <figure class="image is-48x48">
-                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                                </figure>
-                            </div>
                             <div class="media-content">
                                 <p class="title is-4">Tacos 3 Viandes</p>
-                                <p class="subtitle is-6">3 Viandes au choix, 2 Sauces aux choix, Frites & 2 Sauces au choix.</p>
+                                <p class="subtitle is-6">Tacos, 3 Viandes au choix, 2 Sauces aux choix, Frites & 2 Sauces au choix.</p>
                             </div>
                         </div>
-
                         <div class="content">
+                            <br>
+                            <button class="button is-primary is-focused is-medium">Commander</button>
+                            <br>
+                            <time >Temps de Préparation</time>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+
+        <tr>
+            <th>Kebab</th>
+        </tr>
+
+        <!--Ligne Kebab-->
+        <tr>
+            <!--1ère Case/Card-->
+            <td>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4">Kebab #1</p>
+                                <p class="subtitle is-6">Kebab, Sauce au choix, Frites & 2 Sauces au choix.</p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <br>
+                            <button class="button is-primary is-focused is-medium">Commander</button>
+                            <br>
+                            <time >Temps de Préparation</time>
+                        </div>
+                    </div>
+                </div>
+            </td>
+
+            <!--2ème Case/Card-->
+            <td>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4">Kebab #2</p>
+                                <p class="subtitle is-6">Kebab, Sauce au choix, Frites & 2 Sauces au choix.</p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <br>
+                            <button class="button is-primary is-focused is-medium">Commander</button>
+                            <br>
+                            <time >Temps de Préparation</time>
+                        </div>
+                    </div>
+                </div>
+            </td>
+
+            <!--3ère Case/Card-->
+            <td>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4">Kebab #3</p>
+                                <p class="subtitle is-6">Kebab, Sauce au choix, Frites & 2 Sauces au choix.</p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <br>
+                            <button class="button is-primary is-focused is-medium">Commander</button>
+                            <br>
+                            <time >Temps de Préparation</time>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+
+        <tr>
+            <th>Sandwich</th>
+        </tr>
+
+        <!--Ligne Sandwich-->
+        <tr>
+            <!--1ère Case/Card-->
+            <td>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4">Sandwich #1</p>
+                                <p class="subtitle is-6">Sandwich ,Frites & 2 Sauces au choix.</p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <br>
+                            <button class="button is-primary is-focused is-medium">Commander</button>
+                            <br>
+                            <time >Temps de Préparation</time>
+                        </div>
+                    </div>
+                </div>
+            </td>
+
+            <!--2ème Case/Card-->
+            <td>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4">Sandwich #2</p>
+                                <p class="subtitle is-6">Sandwich, Frites & 2 Sauces au choix.</p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <br>
+                            <button class="button is-primary is-focused is-medium">Commander</button>
+                            <br>
+                            <time >Temps de Préparation</time>
+                        </div>
+                    </div>
+                </div>
+            </td>
+
+            <!--3ère Case/Card-->
+            <td>
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4">Sandwich #3</p>
+                                <p class="subtitle is-6">Sandwich, Frites & 2 Sauces au choix.</p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <br>
+                            <button class="button is-primary is-focused is-medium">Commander</button>
                             <br>
                             <time >Temps de Préparation</time>
                         </div>
