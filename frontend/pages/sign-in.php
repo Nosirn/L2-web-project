6a5f7c831fc8a2a44565fd4ff5d1dc5c6a50b721
Bibollet Adrien <71235233+yoratoni@users.noreply.github.com>
2021-11-22 15:55:41 +0100

    Page de SignIn

--- frontend/pages/sign-in.php
+++ frontend/pages/sign-in.php
@@ -1,19 +1,23 @@
-<div class="auth-form px-3" id="sign-in">
-    <div class="ath-form-header p-0">
-        <h1>TITLE</h1>
-    </div>
-    <div class="auth-form-body mt-3">
-        <form action="#" accept-charset="UTF-8" method="post">
-            <label for="login_field"> Email Address </label>
-            <input type="text" name="login_user" id="login_user"
-            autocapitalize="off" autocorrect="off" autocomplete="username"
-            autofocus="autofocus">
+<div class="text-center form-signin" id="sign-in">
+    <img src="https://via.placeholder.com/72" alt class="mb-4" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">
+        Be Thierry. Be Happy.
+    </h1>
 
-            <div class="position-relative">
-                <label for="password"> Password </label>
-                <input type="password" name="password" id="password"
-                autocomplete="current-password">
-            </div>
-        </form>
+    <label for="input_email" class="sr-only">Email address</label>
+    <input type="email" id="input_email" class="form-control"
+    placeholder="Email Address" required autofocus>
+
+    <label for="input_password" class="sr-only">Password</label>
+    <input type="password" id="input_password" class="form-control"
+    placeholder="Password" required>
+
+    <div class="checkbox">
+        <label for="">
+            <input type="checkbox" value="remember-me">
+             Remember Me 
+        </label>
     </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
+    <p class="mt-5 mb-3 text-muted">TEST</p>
 </div>
