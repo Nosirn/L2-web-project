b25c964441d599f2cf95873fbce2ca20946a850a
David Allais <davidallais2001@gmail.com>
2021-11-29 11:01:49 +0100

    Update carousel

--- frontend/pages/common/carousel.php
+++ frontend/pages/common/carousel.php
@@ -1,30 +1,161 @@
-<table class="table-responsive table-borderless">
-    <tr>
-        <td>
-            <div class="card text-white bg-dark mb-3" style="width: 18rem;">
-                <img src="https://via.placeholder.com/1280x720" class="card-img-top" alt="Tacos 8 Viandes">
-                <div class="card-body">
-                    <h5 class="card-title">Tacos 8 Viandes</h5>
-                    <p class="card-text">Plein de bonnes choses</p>
+<div class="box is-flex is-justify-content-space-evenly">               
+                    <div class="card text-white bg-dark mb-3" style="width: 18rem;">
+                        <img src="https://via.placeholder.com/1280x720" class="card-img-top" alt="Tacos 8 Viandes">
+                        <div class="card-body">
+                        <h5 class="card-title">Tacos</h5>
+                        <p class="card-text">1-3 Viandes aux choix, 2 Sauces aux choix, Boissons aux choix</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="dropdown is-active">
+                            <div class="dropdown-trigger">
+                            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu2">
+                                <span>Commander</span>
+                                <span class="icon is-small">
+                                <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                </span>
+                            </button>
+                            </div>
+                            <div class="dropdown-menu" id="dropdown-menu2" role="menu">
+                                <div class="dropdown-content">
+                                    <div class="dropdown-item">
+                                        <button class="button is-dark is-small">1 viande</button>
+                                        <button class="button is-dark is-small">2 viandes</button>
+                                        <button class="button is-dark is-small">3 viandes</button>
+                                    </div>
+                                    <div class="dropdown-item">
+                                        <div class="dropdown">
+                                            <div class="dropdown-trigger">
+                                                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
+                                                <span>1ère Viande</span>
+                                                <span class="icon is-small">
+                                                <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                                </span>
+                                                </button>
+                                            </div>
+                                            <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                                                <div class="dropdown-content">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="dropdown">
+                                            <div class="dropdown-trigger">
+                                                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
+                                                    <span>2ème Viande</span>
+                                                    <span class="icon is-small">
+                                                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                                    </span>
+                                                </button>
+                                            </div>
+                                            <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                                                <div class="dropdown-content">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="dropdown">
+                                            <div class="dropdown-trigger">
+                                            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
+                                                <span>3ème Viande</span>
+                                                <span class="icon is-small">
+                                                <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                                </span>
+                                            </button>
+                                            </div>
+                                            <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                                                <div class="dropdown-content">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                    <hr class="dropdown-divider">
+                                    <div class="dropdown-item">
+                                        <div class="dropdown">
+                                            <div class="dropdown-trigger">
+                                                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
+                                                    <span>Boisson</span>
+                                                    <span class="icon is-small">
+                                                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                                    </span>
+                                                </button>
+                                            </div>
+                                            <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                                                <div class="dropdown-content">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr class="dropdown-divider">
+                                    <div class="dropdown-item">
+                                        <div class="dropdown">
+                                            <div class="dropdown-trigger">
+                                                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
+                                                    <span>1ère Sauce</span>
+                                                    <span class="icon is-small">
+                                                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                                    </span>
+                                                </button>
+                                            </div>
+                                            <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                                                <div class="dropdown-content">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="dropdown">
+                                            <div class="dropdown-trigger">
+                                                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
+                                                    <span>2ème Sauce</span>
+                                                    <span class="icon is-small">
+                                                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                                    </span>
+                                                </button>
+                                            </div>
+                                            <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+                                                <div class="dropdown-content">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr class="dropdown-divider">
+                                    <div class="dropdown-item"></div>
+                                        <button class="button is-dark is-right">Valider</button>
+                                    </div>
+                                    <hr class="dropdown-divider">
+                                    <hr class="dropdown-divider">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+            
+                <div class="card text-white bg-dark mb-3" style="width: 18rem;">
+                    <img src="https://via.placeholder.com/1280x720" class="card-img-top" alt="Tacos 8 Viandes">
+                    <div class="card-body">
+                        <h5 class="card-title">Kebab</h5>
+                        <p class="card-text">Plein de bonnes choses</p>
+                    </div>
+                    <div class="card-body">
+                        <button type="button" class="btn btn-secondary btn-lg">Commander</button>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <button type="button" class="btn btn-secondary btn-lg">Commander</button>
-                </div>
-            </div>
-        </td>
-        
 
-        <td>
-            <div class="card text-white bg-dark mb-3" style="width: 18rem;">
-                <img src="https://via.placeholder.com/1280x720" class="card-img-top" alt="Tacos 8 Viandes">
-                <div class="card-body">
-                    <h5 class="card-title">Tacos 8 Viandes</h5>
-                    <p class="card-text">Plein de bonnes choses</p>
+                <div class="card text-white bg-dark mb-3" style="width: 18rem;">
+                    <img src="https://via.placeholder.com/1280x720" class="card-img-top" alt="Tacos 8 Viandes">
+                    <div class="card-body">
+                        <h5 class="card-title">Burger</h5>
+                        <p class="card-text">Plein de bonnes choses</p>
+                    </div>
+                    <div class="card-body">
+                        <button type="button" class="btn btn-secondary btn-lg">Commander</button>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <button type="button" class="btn btn-secondary btn-lg">Commander</button>
+
+                <div class="card text-white bg-dark mb-3" style="width: 18rem;">
+                    <img src="https://via.placeholder.com/1280x720" class="card-img-top" alt="Tacos 8 Viandes">
+                    <div class="card-body">
+                        <h5 class="card-title">Sandwich</h5>
+                        <p class="card-text">Plein de bonnes choses</p>
+                    </div>
+                    <div class="card-body">
+                        <button type="button" class="btn btn-secondary btn-lg">Commander</button>
+                    </div>
                 </div>
-            </div>
-        </td>
-    </tr>
-</table>
+            </div> 
